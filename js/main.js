25e4208fd7367ec609bd1d37c50ee528a73b53b5
JohnQuimson <john.quimson777@gmail.com>
2023-11-13 16:33:42 +0100

    feat: termine

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const lista = document.querySelector('.list');
+console.log(lista);
+
+const spesa = [
+  'Pane',
+  'Insalata',
+  'Uova',
+  'Pasta',
+  'Riso',
+  'Cozze',
+  'Patate',
+  'Melograno',
+  'Kiwi',
+  'Farina',
+  'Vino rosso',
+];
+
+let j = 0;
+
+while (j < spesa.length) {
+  const prodotto = document.createElement('li');
+  prodotto.classList.add('prodotto');
+  prodotto.textContent = spesa[j];
+  lista.append(prodotto);
+  j++;
+}
